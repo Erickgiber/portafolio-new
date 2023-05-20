a2d13c73ffde0ec28c955ea2bc117395737597b7
Erick Ramirez <erickgiber7@gmail.com>
2023-05-20 01:08:02 -0400

    New improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import type { HeadFC, PageProps } from "gatsby";
 import * as React from "react";
+import { useState } from "react";
 
 const IndexPage: React.FC<PageProps> = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <main>
      <h1> Hello world </h1>
+     <strong> This is a counter: {counter} </strong>
+     <button onClick={() => setCounter(prev => prev + 1)}> +1 to counter </button>
     </main>
   );
 };
