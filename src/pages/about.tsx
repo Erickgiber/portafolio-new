4d40efc678ff4ce70e1759512bb12b9b34270c72
Erick Ramirez <erickgiber7@gmail.com>
2023-05-22 19:05:45 -0400

    New improvements

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { HeadFC } from "gatsby";
 import * as React from 'react';
 import { animate, initial, transition } from "../shared/config";
-import '../shared/styles/index.scss';
+import { IconVenezuela } from '../shared/ui/Icon';
 import { MetaHTML } from "../shared/ui/MetaHTML";
 import { Wrapper } from "../shared/ui/Wrapper";
 
@@ -17,7 +17,31 @@ const AboutPage = () => {
                 animate={animate}
                 transition={transition}
             >
-                <h1 className="title"> About me waos </h1>
+                <div className="left">
+                    <img className="photo" src="/photos/erick.jpg" alt="Erick" />
+                </div>
+                <ul className="right">
+                    <li className="presentation">
+                        Full stack developer
+                    </li>
+                    <li>
+                        My name is Erick Ramírez, Venezuelan <IconVenezuela /> and I&apos;m developer with experience creating websites for small and medium-sized
+                        businesses. Whether you&apos;re trying to gain a job, list your services, or even build an entire
+                        online store, I can help!
+                    </li>
+                    <li>
+                        I have experience in HTML5, CSS3, Sass, Stylus, React JS,
+                        Gatsby JS, Solid JS, Node JS, Socket io, Express JS, Laravel,
+                        Docker, Github, Netlify.
+                    </li>
+                    <li>
+                        I will fully manage your project brief from start to finish
+                    </li>
+                    <li>
+                        Regular communication is very important to me, so let&apos;s keep in touch!
+                    </li>
+                </ul>
+
             </motion.main>
         </Wrapper>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import type { HeadFC, PageProps } from "gatsby";
 import * as React from "react";
-import { animate, initial, initialSkill, transition } from "../shared/config";
+import { animate, initial, transition } from "../shared/config";
 import '../shared/styles/index.scss';
 import { MetaHTML } from "../shared/ui/MetaHTML";
 import { Wrapper } from "../shared/ui/Wrapper";
@@ -24,38 +24,10 @@ const IndexPage: React.FC<PageProps> = () => {
                     <div className="profile">
                         <img className="photo" src="/photos/erick.jpg" alt="Erick"/>
                         <div className="skills">
-                            <motion.span
-                                layout
-                                initial={initialSkill}
-                                animate={animate}
-                                transition={{duration: 0.2}}
-                            >
-                                Full stack developer
-                            </motion.span>
-                            <motion.span
-                                layout
-                                initial={initialSkill}
-                                animate={animate}
-                                transition={{duration: 0.4}}
-                            >
-                                Web designer / Figma
-                            </motion.span>
-                            <motion.span
-                                layout
-                                initial={initialSkill}
-                                animate={animate}
-                                transition={{duration: 0.6}}
-                            >
-                                Software Tester
-                            </motion.span>
-                            <motion.span
-                                layout
-                                initial={initialSkill}
-                                animate={animate}
-                                transition={{duration: 0.8}}
-                            >
-                                QA Automation
-                            </motion.span>
+                            <span>Full stack developer</span>
+                            <span>Web designer / Figma</span>
+                            <span>Software Tester</span>
+                            <span>QA Automation</span>
                         </div>
                     </div>
                 </div>
