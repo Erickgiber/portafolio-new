f5619adde2b763dec2ade57975c35d81aa9645ff
Erick Ramirez <erickgiber7@gmail.com>
2023-05-22 14:16:12 -0400

    New improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,26 @@ import {Header} from "../shared/ui/Header";
 import {Footer} from "../shared/ui/Footer";
 
 const IndexPage: React.FC<PageProps> = () => {
-
     return (
         <>
             <Header/>
-
+            <main className="main-index">
+                <div>
+                    <div className="title">
+                        <span> Hello I&apos;m </span>
+                        <p> Erick Ramirez </p>
+                    </div>
+                    <div className="profile">
+                        <img className="photo" src="/photos/erick.jpg" alt="Erick"/>
+                        <div className="skills">
+                            <span> Full stack developer </span>
+                            <span> Web designer / Figma </span>
+                            <span> Software Tester </span>
+                            <span> QA Automation </span>
+                        </div>
+                    </div>
+                </div>
+            </main>
             <Footer/>
         </>
     );
